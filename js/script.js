7f5f30414d05b58b8c3995c2c0e95ceecfa6c0ea
Fábio Rabello <111283847+FabioRR235@users.noreply.github.com>
2023-08-03 15:14:12 -0300

    falha

--- js/script.js
+++ js/script.js
@@ -59,18 +59,3 @@ window.onscroll = function() {
   }
 
 }
-
-//efeito de aparecer
-const myObserver = new IntersectionObserver((entries) => {
-  entries.forEach( (entry) => {
-    if(entry.isIntersecting){
-        entry.target.classList.add('show')
-    }else{
-        entry.target.classList.remove('show')
-    }
-  })
-})
-
-const elements = document.querySelectorAll('.hidden')
-elements.forEach((element) => myObserver.observe(element))
-
